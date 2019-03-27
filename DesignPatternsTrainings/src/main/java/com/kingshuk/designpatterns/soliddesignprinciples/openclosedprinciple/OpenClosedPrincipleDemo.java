b15e0d54bd0d04280852e9cb199d397039c8b5dd
Kingshuk Mukherjee <sly.mania42@gmail.com>
2019-03-26 22:01:46 -0500

    Small changes

--- DesignPatternsTrainings/src/main/java/com/kingshuk/designpatterns/soliddesignprinciples/openclosedprinciple/OpenClosedPrincipleDemo.java
+++ DesignPatternsTrainings/src/main/java/com/kingshuk/designpatterns/soliddesignprinciples/openclosedprinciple/OpenClosedPrincipleDemo.java
@@ -3,7 +3,6 @@ package com.kingshuk.designpatterns.soliddesignprinciples.openclosedprinciple;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class OpenClosedPrincipleDemo {
 
